9029c9bb9099a59269f8f378d5f6a9e2c95451fa
Júlio Cezar <gitlab@viatechinfo.com.br>
2023-05-09 23:06:08 -0300

    removendo angular material

--- src/app/pagina-inicial/modelo-desktop/modelo-desktop.module.ts
+++ src/app/pagina-inicial/modelo-desktop/modelo-desktop.module.ts
@@ -3,15 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ModeloDesktopComponent } from './modelo-desktop.component';
 import { BarraSuperiorDesktopComponent } from './barra-superior-desktop/barra-superior-desktop.component';
 import { MenuComponent } from './menu/menu.component';
-import { MatTabsModule } from '@angular/material/tabs';
 
 
 
 @NgModule({
   declarations: [ModeloDesktopComponent, BarraSuperiorDesktopComponent, MenuComponent],
   imports: [
-    CommonModule,
-    MatTabsModule
+    CommonModule
   ],
   exports: [
     ModeloDesktopComponent
